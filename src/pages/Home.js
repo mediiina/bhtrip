eb8361452b6044452df195ca1460f8e4e46bb636
mediiina <medinakrupalija11@gmail.com>
2023-08-06 03:23:14 +0200

    updated home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import Header from './Header'
-import Destination from './Destination'
-import Trip from './Trip'
+import Header from '../components/Header'
+import Destination from '../components/Destination'
+import Trip from '../components/Trip'
 
 
 const Home = () => {
