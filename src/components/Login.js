fa97f6d6ed8e0426000cb18f5a8b600cad2e104e
mediiina <medinakrupalija11@gmail.com>
2023-08-07 01:01:14 +0200

    updated authentication

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,55 +3,64 @@ import userLogin from "../auth/userLogin";
 import { useNavigate, useLocation } from "react-router-dom";
 import "./authentication.css"
 
-
-
 const Login = (props) => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [errorMessage, setErrorMessage] = useState(null);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState(null);
 
-    const navigate = useNavigate();
-    const location = useLocation();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const from = location.state && location.state.from && location.state.from.pathname;
+  const { error, login } = userLogin();
 
-    const from = location.state?.from?.pathname || "/dashboard";
-    const { error, login }= userLogin();
-
-    const handleLogin = async (e) => {
-      e.preventDefault();
-      await login(email,password);
-      if (!error) {
-         navigate(from, {replace: true});
-         setEmail("");
-         setPassword("");
-         return;
-      } else {
-         setErrorMessage(error)
-
-      }
-    }
-    return (
-        <div className="login">
-           <h2 className="loginTitle">Login to your account</h2>
-           <form className= "loginForm" onSubmit={handleLogin}>
-              <input className="loginInput"
-                 type="email" 
-                 placeholder="email" 
-                 value={email}
-                 onChange={(e) => setEmail(e.target.value)}/>
-              <input className="loginInput"
-                 type="password" 
-                 placeholder="password"
-                 value={password}
-                 onChange={(e) => setPassword(e.target.value)} 
-             />
-              {error && <p>{errorMessage}</p>}
-              <button className="loginButton"  type="submit"> Login </button>
-           </form>
-           <p className="loginText">Have no an account?</p>
-           <button className="loginButton"  onClick={props.toggleForm}>Sign Up</button>  
-        </div>
-    );
-      
+  const handleLogin = async (e) => {
+   e.preventDefault();
+   if (!email || !password) {
+     setErrorMessage("Please enter your email and password.");
+     return;
+   }
+ 
+   console.log('Logging in...');
+   await login(email, password);
+ 
+   // Proverite da li je prijava uspešna (nema greške)
+   if (!error) {
+     // Ako je prijava uspešna, preusmerite korisnika na odgovarajuću putanju
+     const redirectTo = from || "/dashboard";
+     navigate(redirectTo, { replace: true });
+     setEmail("");
+     setPassword("");
+   } else {
+     // Ako postoji greška pri prijavi, prikažite poruku o grešci
+     setErrorMessage(error);
+   }
+ };
+ 
+  return (
+    <div className="login">
+      <h2 className="loginTitle">Login to your account</h2>
+      <form className="loginForm" onSubmit={handleLogin}>
+        <input
+          className="loginInput"
+          type="email"
+          placeholder="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          className="loginInput"
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        {error && <p>{errorMessage}</p>}
+        <button className="loginButton" type="submit"> Login </button>
+      </form>
+      <p className="loginText">Have no an account?</p>
+      <button className="loginButton" onClick={props.toggleForm}>Sign Up</button>
+    </div>
+  );
 }
 
 export default Login;
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,60 +1,67 @@
 import { useState } from "react";
 import userSignUp from "../auth/userSignUp";
 import { useNavigate, useLocation } from "react-router-dom";
-import "./authentication.css"
-
+import "./authentication.css";
 
 const SignUp = (props) => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [errorMessage, setErrorMessage] = useState(null);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState(null);
 
-    const navigate = useNavigate();
-    const location = useLocation();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const from = location.state && location.state.from && location.state.from.pathname;
 
-    const from = location.state?.from?.pathname || "/dashboard"
+  const { error, signUp } = userSignUp();
 
-    const {error, signUp} = userSignUp();
-
-    const handleSignOut = async (e) => {
-      e.preventDefault();
-
-      await signUp(email, password)
-
-      if(!error) {
-         navigate(from, {replace: true})
-         setEmail("")
-         setPassword("")
-
-         return
-      } else {
-         setErrorMessage(error)
-      }
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+    if (!email || !password) {
+      setErrorMessage("Please enter your email and password.");
+      return;
     }
-    return (
-        <div className="signUp">
-           <h2 className="signUpTitle">Create your account</h2>
-           <form className= "signUpForm"  onSubmit={handleSignOut}>
-              <input className="signUpInput"
-                 type="email" 
-                 placeholder="email"
-                 value={email}
-                 onChange={(e) => setEmail(e.target.value)} 
-                  />
-              <input className="signUpInput"
-                 type="password" 
-                 placeholder="password"
-                 value={password}
-                 onChange={(e) => setPassword(e.target.value)} />
-              {error && <p>{errorMessage}</p>}
-              <button className="signUpButton" type="submit"> Sign Up </button>
-           </form>
-           <p className="signUpText" >Have an account?</p>
-           <button className="signUpButton" onClick={props.toggleForm}>Login</button> 
-        </div>
-    );
-        
-    
+
+    console.log('Signing up...');
+    const res = await signUp(email, password);
+
+    // Proverite da li je registracija uspešna (nema greške)
+    if (!error) {
+      // Ako je registracija uspešna, preusmerite korisnika na odgovarajuću putanju
+      const redirectTo = from || "/dashboard";
+      navigate(redirectTo, { replace: true });
+      setEmail("");
+      setPassword("");
+    } else {
+      // Ako postoji greška pri registraciji, prikažite poruku o grešci
+      setErrorMessage(error);
+    }
+  };
+
+  return (
+    <div className="signUp">
+      <h2 className="signUpTitle">Create your account</h2>
+      <form className="signUpForm" onSubmit={handleSignUp}>
+        <input
+          className="signUpInput"
+          type="email"
+          placeholder="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          className="signUpInput"
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        {error && <p>{errorMessage}</p>}
+        <button className="signUpButton" type="submit"> Sign Up </button>
+      </form>
+      <p className="signUpText">Have an account?</p>
+      <button className="signUpButton" onClick={props.toggleForm}>Login</button>
+    </div>
+  );
 }
 
 export default SignUp;
